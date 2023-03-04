4931e33df187c31c6213973d49d4f20f042e0bcf
physicksKim <physicksKim@gmail.com>
2023-03-04 16:12:14 +0900

    made repo

---
+++ src/main/java/physicks/secondBoard/SecondBoardApplication.java
@@ -0,0 +1,14 @@
+package physicks.secondBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondBoardApplication.class, args);
+	}
+	// jdbc:h2:tcp://localhost/~/querydsl
+
+}
---
+++ src/test/java/physicks/secondBoard/SecondBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package physicks.secondBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
