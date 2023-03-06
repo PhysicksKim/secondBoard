889fdc1a04e65f998aa7560bd34cb6a762c26000
physicksKim <physicksKim@gmail.com>
2023-03-06 17:50:16 +0900

    Oauth config codes

---
+++ src/main/java/physicks/secondBoard/config/oauth/SessionUser.java
@@ -0,0 +1,19 @@
+package physicks.secondBoard.config.oauth;
+
+import lombok.Getter;
+import physicks.secondBoard.domain.user.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
