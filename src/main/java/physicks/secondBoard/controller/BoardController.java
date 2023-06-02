f7f1139dde165549a83310a9fd1e8305710ec377
physicksKim <physicksKim@gmail.com>
2023-06-02 22:54:17 +0900

    refactor : group common URLs in BoardController

--- src/main/java/physicks/secondBoard/controller/BoardController.java
+++ src/main/java/physicks/secondBoard/controller/BoardController.java
@@ -5,16 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import physicks.secondBoard.domain.board.BoardPostListDto;
 import physicks.secondBoard.domain.board.BoardService;
 import physicks.secondBoard.domain.post.Post;
 
 import java.util.List;
 
+@RequestMapping("/board")
 @Controller
 @RequiredArgsConstructor
 @Slf4j
@@ -22,21 +20,29 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping("/board")
+    @GetMapping
     public String boardMain(Model model, Pageable pageable) {
         List<BoardPostListDto> postList = boardService.getBoardPostList(pageable);
         model.addAttribute("postList", postList);
         return "board";
     }
 
-    @GetMapping("/board/{id}")
+    @GetMapping("/{id}")
     public String boardPostRead(@PathVariable Long id, Model model) {
         Post findPost = boardService.findPostById(id);
         model.addAttribute("post", findPost);
         return "post";
     }
 
-    @PostMapping("/board")
+    @GetMapping("/write")
+    public String boardPostWritePage() {
+        // !!! 구현 필요 !!!
+        // 1. Post Write Test 먼저 구현
+        // 2. Post Write 로직과 Page 구현 필요
+        return "write";
+    }
+
+    @PostMapping
     @ResponseBody
     public String writePost(String title, String author, String content) {
         Post post = new Post(title, author, content);
